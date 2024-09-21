b856dedfc5e9c3f23f6df38cec04d2c2a26cae67
Ali Hassan <142111910+SyedAliHassanNaqvi@users.noreply.github.com>
2024-09-21 17:13:38 +0500

    Github Link
    Added my github profile link

--- Projects/reactRouter/src/components/github/Github.jsx
+++ Projects/reactRouter/src/components/github/Github.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom";
-
+import { Link, useLoaderData } from "react-router-dom";
+let gitId = "SyedAliHassanNaqvi";
 function Github() {
+  let gitId = "SyedAliHassanNaqvi";
   const data = useLoaderData();
   /*const [data, setData] = useState([]);
   useEffect(() => {
@@ -18,6 +19,16 @@ function Github() {
       <div className="bg-red-500 text-white p-4 text-3xl">
         Github Followers: {data.followers}
         <img src={data.avatar_url} alt="Git Picture" width={300} />
+        <h3>
+          <Link
+            className="font-bold"
+            to={`https://github.com/${gitId}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit my GitHub Profile
+          </Link>
+        </h3>
       </div>
     </>
   );
@@ -25,8 +36,6 @@ function Github() {
 export default Github;
 //loader is basically used to optimize the loading time when fetching data from an Api or db. When we hover on GitHub button on header, Loader loads the data from api and stores in cache. So even before clicking on the button our data is already available which reduces lag(loading time)
 export const githubInfoLoader = async () => {
-  const response = await fetch(
-    "https://api.github.com/users/SyedAliHassanNaqvi"
-  );
+  const response = await fetch(`https://api.github.com/users/${gitId}`);
   return response.json();
 };
