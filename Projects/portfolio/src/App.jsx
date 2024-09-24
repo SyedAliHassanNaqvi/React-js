79bd83301db83d371a9aa0957b4d9f643b6c5a38
Ali Hassan <142111910+SyedAliHassanNaqvi@users.noreply.github.com>
2024-09-24 23:51:10 +0500

    Nav Bar button making and fabar
    setup of navbar

---
+++ Projects/portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+// { } is used to write js
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
